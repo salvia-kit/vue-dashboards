6155a716d47c2fba4deade167c655177450f9e8e
enochndika <enochbeloved@gmail.com>
2021-07-14 22:35:57 +0100

    remove index.css in src repertory

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './routes';
-import './index.css';
+import 'tailwindcss/tailwind.css';
 
 createApp(App).use(router).mount('#app');
