befe3ba24cc7353aaecde3410e9d878d69f51444
Enoch Ndika <enochbeloved@gmail.com>
2023-07-14 17:23:18 +0100

    add dashboard v3

---
+++ dashboard-v3/src/pages/admin/time-manage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Content from "../../components/Content.vue";
+</script>
+
+<template>
+  <Content title="Time Manage" />
+</template>
---
+++ dashboard-v3/tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{js,vue,ts}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
